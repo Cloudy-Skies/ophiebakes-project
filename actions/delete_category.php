25039ab6d1ad025c1de42dd44e60ce8d96e4e31a
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-12-23 00:54:05 +0000

    delete category stuff

---
+++ actions/delete_category.php
@@ -0,0 +1,21 @@
+<?php
+
+require("../controllers/product_controller.php");
+require('../settings/core.php');
+
+if(isset($_GET['cat_id'])){
+    $cat_id = $_GET['cat_id'];
+    
+
+    // call the delete one category controller
+    $result = delete_one_category_ctr($cat_id);
+
+    if($result){
+        header('Location: ../admin/view_cat.php');
+
+    }
+     else {
+        echo ("<script>alert('Could not delete category, try again.'); window.location.href = '../admin/categories.php';</script>");
+     }
+
+}
