0f278d900a05b7ba18a291f00fa0e4678f4aa9f1
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-12-23 03:38:48 +0000

    core page settled

--- settings/core.php
+++ settings/core.php
@@ -6,13 +6,26 @@ session_start();
 ob_start();
 
 //funtion to check for login
-
+function check_login(){
+	//check if login session exit
+	if (!isset($_SESSION['user_id'])) 
+	{
+		//redirect to login page
+    	header('Location: ../login/login.php');
+	}
+}
 
 //function to get user ID
 
 
 //function to check for role (admin, customer, etc)
-
+function check_permission(){
+	//get session role
+	if (isset($_SESSION['user_role'])) {
+		//assign session to an array
+		return $_SESSION['user_role'];
+	}
+}
 
 
 ?>
