1d4adf84249e4860cdac88e81f4fbd689e6cb0b6
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-12-23 09:06:52 +0000

    cha;e

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,12 +1,11 @@
 <?php
 //Database credentials
-// define('SERVER', 'us-cdbr-east-06.cleardb.net');
-// define('USERNAME','bc50cf1b46f2a6');
-// define('PASSWORD', '791c7e0d');
-// define('DATABASE', 'heroku_4351a7d9a2694af');
+define('SERVER', 'us-cdbr-east-06.cleardb.net');
+define('USERNAME','bc50cf1b46f2a6');
+define('PASSWORD', '791c7e0d');
+define('DATABASE', 'heroku_4351a7d9a2694af');
 
-define('SERVER', 'localhost');
-define('USERNAME','root');
-define('PASSWORD', '');
-define('DATABASE', 'bakery');
-?>
+// define('SERVER', 'localhost');
+// define('USERNAME','root');
+// define('PASSWORD', '');
+// define('DATABASE', 'bakery');
